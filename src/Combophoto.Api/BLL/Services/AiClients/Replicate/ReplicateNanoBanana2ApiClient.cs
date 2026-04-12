9be800c65b5bf1cceba44e7faf57491c737bd4ef
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-04-12 23:50:10 +0300

    add prompt

---
+++ src/Combophoto.Api/BLL/Services/AiClients/Replicate/ReplicateNanoBanana2ApiClient.cs
@@ -0,0 +1,17 @@
+﻿namespace Combophoto.Api.BLL.Services.AiClients.Replicate
+{
+    public class ReplicateNanoBanana2ApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        private readonly ILogger<ReplicateNanoBanana2ApiClient> _logger;
+    
+        public ReplicateNanoBanana2ApiClient(
+            HttpClient httpClient,
+            ILogger<ReplicateNanoBanana2ApiClient> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+    }
+}
