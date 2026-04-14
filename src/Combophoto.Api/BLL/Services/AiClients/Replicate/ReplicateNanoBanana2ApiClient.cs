5d70aee36e8f11c3646c453179531fdac1a15153
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-04-14 21:22:11 +0300

    add ProcessPredictionAsync

--- src/Combophoto.Api/BLL/Services/AiClients/Replicate/ReplicateNanoBanana2ApiClient.cs
+++ src/Combophoto.Api/BLL/Services/AiClients/Replicate/ReplicateNanoBanana2ApiClient.cs
@@ -1,4 +1,6 @@
 ﻿using Combophoto.Api.BLL.Abstract;
+using System.Text;
+using System.Text.Json;
 
 namespace Combophoto.Api.BLL.Services.AiClients.Replicate
 {
@@ -17,6 +19,50 @@ namespace Combophoto.Api.BLL.Services.AiClients.Replicate
             _httpClient = httpClient;
             _promptService = promptService;
             _logger = logger;
+        }
+
+        public async Task<string> ProcessPredictionAsync(string[] imageUrls)
+        {
+            //Create request
+            var requestBody = new
+            {
+                input = new
+                {
+                    prompt = _promptService.GetPrompt(),
+                    //ompt = "In the first photo, there is a person. In the second photo, there is a hairstyle of another person. Transfer the hairstyle from the second photo onto the head of the person in the first photo. Keep the face, pose, and background from the first photo, but make the hair the same as in the second photo (shape, length, color, style). The result should look as realistic and natural as possible.",
+                    image_input = imageUrls                    
+                }
+            };
+
+            //Serialize request
+            var json = JsonSerializer.Serialize(requestBody);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            //Send request            
+            var response = await _httpClient.PostAsync("models/google/nano-banana-2/predictions", content);
+            if (!response.IsSuccessStatusCode)
+            {
+                var responseContentError = await response.Content.ReadAsStringAsync();
+                _logger.LogError(responseContentError);
+                string httpStatusCode = ((int)response.StatusCode).ToString();
+                return null;
+            }
+
+            //Read and parse response
+            var responseContent = await response.Content.ReadAsStringAsync();
+            using var document = JsonDocument.Parse(responseContent);
+            if (document.RootElement.TryGetProperty("output", out var outputProperty))
+            {
+                var resultUrl = outputProperty.GetString();
+                if (!string.IsNullOrEmpty(resultUrl))
+                {
+                    return resultUrl;
+                }
+
+                _logger.LogError(responseContent);
+            }
+
+            return null;
         }
     }
 }
---
+++ src/Combophoto.Api/Models/CombophotoRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Combophoto.Api.Models
+{
+    public class CombophotoRequest
+    {
+        public string[] ImageUrls { get; set; }
+    }
+}
