944c0e768c8a8d652190c1d0279341275570d0bc
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-04-14 22:24:58 +0300

    add MergeController

---
+++ src/Combophoto.Api/Controllers/MergeController.cs
@@ -0,0 +1,30 @@
+﻿using Combophoto.Api.BLL.Abstract;
+using Combophoto.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Combophoto.Api.Controllers
+{
+    [Route("merge")]
+    [ApiController]
+    public class MergeController : ControllerBase
+    {
+        private readonly IReplicateNanoBanana2ApiClient _apiClient;
+
+        public MergeController(IReplicateNanoBanana2ApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        [HttpPost("run")]
+        public async Task<ActionResult> Run(CombophotoRequest request)
+        {
+            var resUrl = await _apiClient.ProcessPredictionAsync(request.ImageUrls);
+            if (string.IsNullOrEmpty(resUrl))
+            {
+                return BadRequest();
+            }
+
+            return Ok(resUrl);
+        }
+    }
+}
